0c15a0a968386dd9c4e44b4cd3ba2b5b1890f44c
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2024-04-11 15:30:03 +0700

    Add project files.

---
+++ bmkg-soetta.cpp
@@ -0,0 +1,12 @@
+﻿// bmkg-soetta.cpp : Defines the entry point for the application.
+//
+
+#include "bmkg-soetta.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
